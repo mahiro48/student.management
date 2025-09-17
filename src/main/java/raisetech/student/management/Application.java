044f8edd0075f0627d8f8010793232ce8e64f0a7
mahiro <ochiai.mahiro4869@icloud.com>
2025-09-17 19:31:04 +0900

    Make Application controller thread-safe using AtomicReference

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -2,43 +2,38 @@ package raisetech.student.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
-	private String name = "Mahiro Ochiai";
-	private String age = "27";
+	// スレッドセーフなフィールドに変更
+	private final AtomicReference<String> name = new AtomicReference<>("Mahiro Ochiai");
+	private final AtomicReference<String> age = new AtomicReference<>("27");
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(Application.class, args);
-
 	}
 
 	@GetMapping("/studentInfo")
 	public String getStudentInfo() {
-		return name + " " + age + "歳";
+		return name.get() + " " + age.get() + "歳";
 	}
 
 	@PostMapping("/studentInfo")
-	public void setStudentInfo(String name, String age) {
-		this.name = name;
-		this.age = age;
+	public void setStudentInfo(@RequestParam String name, @RequestParam String age) {
+		this.name.set(name);
+		this.age.set(age);
 	}
 
 	@PostMapping("/studentName")
-	public void updateStudentName(String name) {
-		this.name = name;
+	public void updateStudentName(@RequestParam String name) {
+		this.name.set(name);
 	}
-
-
 }
-
 
 
 
