130ed8c52d20bccfabb02882572d823a4a939a77
mahiro <ochiai.mahiro4869@icloud.com>
2025-11-14 17:48:04 +0900

    StudentCourses マッピング修正：id フィールド対応

--- src/main/java/raisetech/student/management/StudentRepository.java
+++ src/main/java/raisetech/student/management/StudentRepository.java
@@ -3,8 +3,7 @@ package raisetech.student.management;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Result;
+
 
 @Mapper
 public interface StudentRepository {
